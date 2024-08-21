4dbb5e85d28c4d6952f552374937344ebb6f0200
Juan Fernandes <juanzitow2@gmail.com>
2024-08-21 09:39:43 -0300

    refactor: added validations to update farm

--- src/api-hooks/farms/use-update-farm.ts
+++ src/api-hooks/farms/use-update-farm.ts
@@ -1,20 +1,21 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { updateFarm } from '../../features/farms-slice';
-import FarmType from '../../types/farm-type';
+import FarmType, { validateFarm } from '../../types/farm-type';
 
 type UseUpdateFarmProps = {
   onSuccess?: (data: FarmType) => void;
-  onError?: (error: any) => void;
 };
 
-function useUpdateFarm({ onSuccess, onError }: UseUpdateFarmProps) {
+function useUpdateFarm({ onSuccess }: UseUpdateFarmProps) {
+  const [errors, setErrors] = useState<any>({});
   const dispatch = useDispatch();
 
   return {
     fetch: (farm: FarmType) => {
-      //check for errors here
-      if (!farm?.name) {
-        onError?.({ message: 'Name is required' });
+      const errors = validateFarm(farm);
+      if (Object.keys(errors).length) {
+        setErrors(errors);
         return;
       }
 
@@ -22,6 +23,7 @@ function useUpdateFarm({ onSuccess, onError }: UseUpdateFarmProps) {
       onSuccess?.(farm);
     },
     pending: false,
+    errors,
   };
 }
 
